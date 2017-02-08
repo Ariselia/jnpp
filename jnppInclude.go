a2b1c5ca23a75f8f68cbeecdbc90e4aa50182502
Shelikhoo <xiaokangwang@outlook.com>
2017-02-08 09:40:30 +0800

    Refactor

--- jnppInclude.go
+++ jnppInclude.go
@@ -8,27 +8,30 @@ import (
 )
 
 func (jn *Jnpp) include() {
-	err := jnpputil.FindIf("#!include", jn.jn, func(fi []string, i interface{}) error {
-		currentname := fi[len(fi)-1]
-		nextname := strings.Split(currentname, "|")[1]
-		merging := jn.jn.GetPath(fi...).MustString(nextname + ".json")
-
-		incJn := new(Jnpp)
-		incJn.basedir = filepath.Dir(jn.basedir + "/" + merging)
-		incJn.environment = jn.environment
-		//spew.Dump(merging, incJn)
-		err := incJn.Parse(merging)
-		if err != nil {
-			return err
-		}
-
-		cw := jn.jn.GetPath(fi[:len(fi)-1]...)
-		cw.Set(nextname, incJn.jn.MustMap())
-
-		cw.Del(currentname)
-		return nil
+	err := jnpputil.FindIf("#!Include", jn.jn, func(fi []string, i interface{}) error {
+		return jn.includeat(fi, i)
 	}, nil)
 	if err != nil {
 		panic(err)
 	}
+}
+func (jn *Jnpp) includeat(fi []string, i interface{}) error {
+	currentname := fi[len(fi)-1]
+	nextname := strings.Split(currentname, "|")[1]
+	merging := jn.jn.GetPath(fi...).MustString(nextname + ".json")
+
+	incJn := new(Jnpp)
+	incJn.basedir = filepath.Dir(jn.basedir + "/" + merging)
+	incJn.environment = jn.environment
+	//spew.Dump(merging, incJn)
+	err := incJn.Parse(merging)
+	if err != nil {
+		return err
+	}
+
+	cw := jn.jn.GetPath(fi[:len(fi)-1]...)
+	cw.Set(nextname, incJn.jn.MustMap())
+
+	cw.Del(currentname)
+	return nil
 }
--- jnppMerge.go
+++ jnppMerge.go
@@ -7,25 +7,29 @@ import (
 )
 
 func (jn *Jnpp) merge() {
-	jnpputil.FindIf("#!merge", jn.jn, func(fi []string, i interface{}) error {
-		currentname := fi[len(fi)-1]
-		//nextname := strings.Split(currentname, "|")[1]
-		merging := jn.jn.GetPath(fi...).MustString()
-
-		incJn := new(Jnpp)
-		incJn.basedir = filepath.Dir(jn.basedir + "/" + merging)
-		incJn.environment = jn.environment
-		err := incJn.Parse(merging)
-		if err != nil {
-			return err
-		}
-
-		cw := jn.jn.GetPath(fi[:len(fi)-1]...)
-
-		for k, v := range incJn.jn.MustMap() {
-			cw.Set(k, v)
-		}
-		cw.Del(currentname)
-		return nil
+	jnpputil.FindIf("#!Merge", jn.jn, func(fi []string, i interface{}) error {
+		return jn.mergeat(fi, i)
 	}, nil)
+}
+
+func (jn *Jnpp) mergeat(fi []string, i interface{}) error {
+	currentname := fi[len(fi)-1]
+	//nextname := strings.Split(currentname, "|")[1]
+	merging := jn.jn.GetPath(fi...).MustString()
+
+	incJn := new(Jnpp)
+	incJn.basedir = filepath.Dir(jn.basedir + "/" + merging)
+	incJn.environment = jn.environment
+	err := incJn.Parse(merging)
+	if err != nil {
+		return err
+	}
+
+	cw := jn.jn.GetPath(fi[:len(fi)-1]...)
+
+	for k, v := range incJn.jn.MustMap() {
+		cw.Set(k, v)
+	}
+	cw.Del(currentname)
+	return nil
 }
